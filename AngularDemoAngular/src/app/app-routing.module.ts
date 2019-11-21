24ab5ed58916ee2143161ebf147c459ddd0aefca
Kuei Peng <ragnakuei@livemail.tw>
2019-11-21 16:09:23 +0800

    ok

--- AngularDemoAngular/src/app/app-routing.module.ts
+++ AngularDemoAngular/src/app/app-routing.module.ts
@@ -1,29 +1,30 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 // Component
-import { HomeComponent } from './home/home.component';
-import { OrderComponent } from './order/order.component';
-import { OrderListComponent } from './order/order-list/order.list.component';
-import { OrderEditComponent } from './order/order-edit/order-edit.component';
-import { OrderCreateComponent } from './order/order-create/order-create.component';
-import { OrderDeleteComponent } from './order/order-delete/order-delete.component';
+import { HomeComponent } from "./home/home.component";
+import { OrderComponent } from "./order/order.component";
+import { OrderListComponent } from "./order/order-list/order.list.component";
+import { OrderEditComponent } from "./order/order-edit/order-edit.component";
+import { OrderCreateComponent } from "./order/order-create/order-create.component";
+import { OrderDeleteComponent } from "./order/order-delete/order-delete.component";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: "", component: HomeComponent },
   {
-    path: 'order', component: OrderComponent
-    , children: [
-      { path: '', component: OrderListComponent },
-      { path: 'create', component: OrderCreateComponent },
-      { path: 'edit/:id', component: OrderEditComponent },
-      { path: 'delete/:id', component: OrderDeleteComponent },
+    path: "order",
+    component: OrderComponent,
+    children: [
+      { path: "", component: OrderListComponent },
+      { path: "create", component: OrderCreateComponent },
+      { path: "edit/:id", component: OrderEditComponent },
+      { path: "delete/:id", component: OrderDeleteComponent }
     ]
-  },
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
